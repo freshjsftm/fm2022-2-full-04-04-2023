1652fbfc28411d601a46e127989226ff9438778f
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-04-05 10:11:41 +0300

    Add query-string

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,10 +1,20 @@
 import axios from "axios";
+import qs from "query-string";
 
 const httpClient = axios.create({
   baseURL: "http://localhost:3000/api",
 });
 
-export const getUsers = ({ limit, offset }) =>
-  httpClient.get("/users?limit=2&offset=40");
+export const getUsers = (options={}) => {
+  const defaultOptions = {
+    limit:5,
+    offset:0
+  }
+  const readyOptions = {
+    ...defaultOptions,
+    ...options
+  }
+  return httpClient.get(`/users?${qs.stringify(readyOptions)}`);
+};
 
 export const postUser = (values) => httpClient.post("/users", values);
