6e0bde0f5cb10f2cd7c536ad926e6e91b73d56dc
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-04-06 11:51:35 +0300

    Add lazy load

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import UserPage from "./pages/UserPage";
-import GroupPage from "./pages/GroupPage";
 import NavMenu from "./components/NavMenu";
-import UserProfile from "./components/UserProfile";
+const HomePage = lazy(() => import("./pages/HomePage"));
+const UserPage = lazy(() => import("./pages/UserPage"));
+const GroupPage = lazy(() => import("./pages/GroupPage"));
+const UserProfile = lazy(() => import("./components/UserProfile"));
 
 function App() {
   return (
     <BrowserRouter>
       <NavMenu />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/users" element={<UserPage />} />
-        <Route path="/groups" element={<GroupPage />} />
-        <Route path="/users/:idUser" element={<UserProfile />} />
-        {/* <Route path="/tasks" element={<TaskPage />} /> */}
-      </Routes>
+      <Suspense fallback={'Loading....'}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/users" element={<UserPage />} />
+          <Route path="/groups" element={<GroupPage />} />
+          <Route path="/users/:idUser" element={<UserProfile />} />
+          {/* <Route path="/tasks" element={<TaskPage />} /> */}
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
